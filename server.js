d3e8ff0c6e2b7f2e47e5874419cb2135edee5732
Felipe Bravo <119537502+PhillipBr@users.noreply.github.com>
2024-09-09 18:32:07 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,18 @@ require('dotenv').config({ path: './sql.env' });
 const app = express();
 const PORT = process.env.PORT || 3002;
 
-app.use(cors());
+// Configuración de CORS para permitir solicitudes desde GitHub Pages
+const corsOptions = {
+    origin: 'https://phillipbr.github.io', // Cambia esto por tu dominio específico si es necesario
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+// Servir archivos estáticos
 app.use('/css', express.static(path.join(__dirname, 'CSS')));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
+// Configuración de conexión a MySQL
 const db = mysql.createConnection({
     host: process.env.MYSQL_ADDON_HOST,
     user: process.env.MYSQL_ADDON_USER,
@@ -22,16 +30,17 @@ const db = mysql.createConnection({
 db.connect(err => {
     if (err) {
         console.error('Error connecting to MySQL: ' + err.message);
-        process.exit(1); // Exit in case of DB connection error
+        process.exit(1); // Salir en caso de error de conexión a la base de datos
     }
     console.log('Connected to the MySQL server.');
 });
 
+// Endpoint para servir el HTML principal
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-
+// Endpoint API para obtener canciones
 app.get('/api/songs', (req, res) => {
     const sql = `SELECT AR.SongID, AR.Title, AR.Artist, TS.Album, TS.Popularity, TS.Duration, TS.CoverImage, TS.ReleaseDate, TS.Genre
                  FROM AR
