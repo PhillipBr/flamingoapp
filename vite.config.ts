35434e6b2bd57793e53f9b1edd1cfa71f6da4c9c
Felipe Bravo <119537502+PhillipBr@users.noreply.github.com>
2024-09-10 19:14:35 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/flamingoapp/', 
+  base: '/flamingoapp/',  
   plugins: [react()],
-  server: {
-    port: 3000, 
-    strictPort: true 
-  },
   build: {
-    outDir: 'dist', 
-    sourcemap: true 
+    outDir: 'dist',  
+    rollupOptions: {
+      input: '/src/main.tsx'
+    }
   }
 });
