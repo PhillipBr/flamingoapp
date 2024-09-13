514702df917abfbe652fd0d7875d8830d7a03c2a
Felipe Bravo <119537502+PhillipBr@users.noreply.github.com>
2024-09-12 22:39:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// Primero, carga las variables de entorno desde el archivo sql.env
 require('dotenv').config({ path: './sql.env' });
 
 const express = require('express');
@@ -6,30 +5,41 @@ const mysql = require('mysql');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3002; // Usa el puerto proporcionado por el entorno o 3002 si no está definido.
+const PORT = process.env.PORT || 3002;
 
-app.use(cors()); // Habilita CORS para todas las rutas y orígenes.
+app.use(cors());
 
-// Configuración de conexión MySQL utilizando variables de entorno
-const db = mysql.createConnection({
-    host: process.env.MYSQL_ADDON_HOST,
-    user: process.env.MYSQL_ADDON_USER,
-    password: process.env.MYSQL_ADDON_PASSWORD,
-    database: process.env.MYSQL_ADDON_DB,
-    port: process.env.MYSQL_ADDON_PORT
-});
+var connection;
 
+function handleDisconnect() {
+    connection = mysql.createConnection({ 
+        host: process.env.MYSQL_ADDON_HOST,
+        user: process.env.MYSQL_ADDON_USER,
+        password: process.env.MYSQL_ADDON_PASSWORD,
+        database: process.env.MYSQL_ADDON_DB,
+        port: process.env.MYSQL_ADDON_PORT
+    });
 
-// Conectar a MySQL
-db.connect(err => {
-    if (err) {
-        console.error('Error connecting: ' + err.message);
-        return;
-    }
-    console.log('Connected to the MySQL server.');
-});
+    connection.connect(err => {
+        if (err) {
+            console.error('Error connecting to MySQL:', err);
+            setTimeout(handleDisconnect, 2000); 
+        }
+        console.log('Connected to MySQL server.');
+    });
 
-// Archivos estáticos (ajusta según tu estructura de carpetas)
+    connection.on('error', err => {
+        console.error('MySQL error:', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect(); 
+        } else {
+            throw err;
+        }
+    });
+}
+
+handleDisconnect(); 
+
 app.use(express.static('public'));
 
 app.get('/api/songs', (req, res) => {
@@ -37,16 +47,15 @@ app.get('/api/songs', (req, res) => {
                  FROM AR
                  JOIN TS ON AR.SongID = TS.SongID
                  ORDER BY AR.Views DESC`;
-    db.query(sql, (error, results, fields) => { // Asegúrate de que 'error' esté definido correctamente aquí
+    connection.query(sql, (error, results, fields) => {
         if (error) {
-            console.error('Error fetching data: ' + error.message);
+            console.error('Error fetching data:', error.message);
             res.status(500).send('Error fetching data');
             return;
         }
         res.json(results);
     });
 });
-
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
